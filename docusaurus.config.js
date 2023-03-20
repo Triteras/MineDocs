36458bdbdfce7b76630c925e9142c347640a8dc7
Louan Fontenele <louanbastos61@gmail.com>
2023-03-20 14:51:45 -0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,6 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "Triteras", // Usually your GitHub org/user name.
   projectName: "MineDocs", // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onDuplicateRoutes: "throw",

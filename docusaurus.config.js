625596de62e886408544d6a0c68763acfebc6845
Louan Fontenele <louanbastos61@gmail.com>
2023-03-20 19:53:25 -0300

    Removido e adicionado coisas
    Removido o Footer, Adicionado os icones da navbar, removido o autor e ultima atuliização e removido a possibilidade de editar arquivos

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,15 +44,15 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: `https://github.com/${organizationName}/${projectName}/tree/main/`,
-          showLastUpdateAuthor: true,
-          showLastUpdateTime: true,
+          //editUrl: `https://github.com/${organizationName}/${projectName}/tree/main/`,
+          showLastUpdateAuthor: false,
+          showLastUpdateTime: false,
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: `https://github.com/${organizationName}/${projectName}/tree/main/`,
+          //editUrl: `https://github.com/${organizationName}/${projectName}/tree/main/`,
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -111,7 +111,12 @@ const config = {
           { to: "/blog", label: "Blog", position: "left" },
           {
             href: "https://github.com/Triteras/MineDocs",
-            label: "GitHub",
+            className: "navbar-item-github",
+            position: "right",
+          },
+          {
+            href: "https://discordapp.com/invite/A2mzgJgrmj",
+            className: "navbar-item-discord",
             position: "right",
           },
         ],

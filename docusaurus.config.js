460dcde885ad49768354ec0933aa51b8663b04ae
Louan Fontenele <louanbastos61@gmail.com>
2023-03-20 15:11:13 -0300

    Consertado uns links bugados!

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,8 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
 
-          editUrl:
-            "https://github.com/Triteras/MineDocs/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/facebook/docusaurus/blob/main/",
         },
         blog: {
           showReadingTime: true,
